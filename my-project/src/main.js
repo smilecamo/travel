36dd15938938b543c1ec58b2a35250d574ad39be
smilecamo <1191126635@qq.com>
2018-06-28 22:02:44 +0800

    初始化项目

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -3,9 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决click300ms问题  npm install fastclick --save
+import fastclick from 'fastclick'
+// 初始化css
+import './assets/styles/reset.css'
+// 解决1border问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+// 把fastclick绑定到body上
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- my-project/src/pages/home/home.vue
+++ my-project/src/pages/home/home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    hello
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'home'
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
